b1188014c42ec31bc4c495b265b4804a4c906aee
Jacob Slattery <timberwolf@bellsouth.net>
2017-09-25 11:16:25 -0400

    Added code to print the number of links that are about to be searched and count each link as it is printed

--- PythonWikiHitlerApplication1/PythonWikiHitlerApp/WikiHitlerApp.py
+++ PythonWikiHitlerApplication1/PythonWikiHitlerApp/WikiHitlerApp.py
@@ -1,15 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
-import threading
+import sys
 
-hitlerLink = '/wiki/Adolf_Hitler'
-wikiLink = '/wiki/'
-
+hitlerLink = 'https://en.wikipedia.org/wiki/Adolf_Hitler'
 
 
 #Gets all the links on the page body, and deletes the ref list, then returns a list of the links
 def getWikiPageLinks(webpageURL):
+    if not webpageURL:
+        return False
     linkList = []
     page = requests.get(webpageURL)
     soup = BeautifulSoup(page.content, 'html.parser')
@@ -18,39 +18,35 @@ def getWikiPageLinks(webpageURL):
         div.decompose()
     for link in body.find_all('a', href=True):
         hrefLink = link.get('href')
-        if hrefLink.startswith(wikiLink):
+        if hrefLink.startswith('/wiki/'):
             fullLink = urllib.parse.urljoin('https://en.wikipedia.org', hrefLink)
             linkList.append(fullLink)
+            masterList.append(fullLink)
     return list(set(linkList))
-
-#Testing around with grequesets
-"""def goThroughEachLinkInList(linkList):
-    levelTwoList = []
-    for link in sorted(linkList, key=str.lower):
-        fullLink = urllib.parse.urljoin('https://en.wikipedia.org', link)
-        pageLinks = getWikiPageLinks(fullLink)
-        levelTwoList.append(pageLinks)
-    
-    rs = (grequests.get(url) for url in levelTwoList)
-
-    print(grequests.map(rs))"""
 
 #Gets each link inside the list, turns them into urls, sorts them, prints each to screen, and sifts through their links for hitler
 #Program stops if hitler is found
 def goThroughEachLinkInList(linkList):
-    levelTwoList = []
-    for link in sorted(linkList, key=str.lower):
+    if not linkList:
+        return False
+    nextLevelList = []
+    sortedList = sorted(list(set(linkList)), key=str.lower)
+    print('There are ' + str(len(sortedList)) + ' links to search')
+    count = 0
+    for link in sortedList:
+        count += 1
+        print(str(count) + '. ' + link)
         pageLinks = getWikiPageLinks(link)
-        levelTwoList.append(pageLinks)
-        print(link)
+        nextLevelList.append(pageLinks)
         if isHitlerInList(pageLinks):
-            return levelTwoList
-    return levelTwoList
+            return nextLevelList
+    return nextLevelList
 
 #Simply checks if hitler is in the list
 def isHitlerInList(linkList):
     for link in linkList:
         if hitlerLink in link:
+            #writeNewDirectLinkInFile(link)
             return True
     return False
 
@@ -64,18 +60,15 @@ def findHitler():
         print('Site Directly Links To Hitler!')
         return
     print('Checking Second Level...')
-    #newList = Pool(processes=4).map(goThroughEachLinkInList, myList)
-    #newList = ThreadPool(2).imap(goThroughEachLinkInList, myList) This is throwing an error
-    newList = goThroughEachLinkInList(myList) #This works but very slow
-    """threads = [threading.Thread(target=goThroughEachLinkInList, args=(url,)) for url in myList]
-    for thread in threads:
-        thread.start()
-    for thread in threads:
-        thread.join()"""
-    if isHitlerInList(threads):
-        print('Site Directly Links To Hitler!')
+    if isHitlerInList(goThroughEachLinkInList(list(set(masterList)))):
+        print('Hitler Was Found!')
+        return
+    print('Checking Third Level...')
+    if isHitlerInList(goThroughEachLinkInList(list(set(masterList)))):
+        print('Hitler Was Found!')
         return
     else:
-        print('Hitler Not Found')
+        print('Hitler not found')
+
 
 findHitler()
